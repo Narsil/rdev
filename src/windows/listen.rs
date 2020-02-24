89f3f1a330b844f32fff103885eee6cd0557a748
Nicolas Patry <patry.nicolas@protonmail.com>
2020-02-24 00:56:55 -0800

    Windows seems done..
    Very odd implementation but internet seems to complain about this functionnality.

--- src/windows/listen.rs
+++ src/windows/listen.rs
@@ -3,21 +3,28 @@ use crate::windows::keycodes::key_from_code;
 use std::ptr::null_mut;
 use std::time::SystemTime;
 use winapi::shared::windef::HHOOK;
+use winapi::shared::minwindef::HKL;
 use winapi::um::errhandlingapi::GetLastError;
+use winapi::um::processthreadsapi::GetCurrentThreadId;
+use winapi::um::winuser;
 use winapi::um::winuser::{
-    CallNextHookEx, GetKeyboardState, GetMessageA, SetWindowsHookExA, ToUnicode, HC_ACTION,
+    CallNextHookEx, GetKeyboardState, GetKeyboardLayout, GetKeyState, GetMessageA, SetWindowsHookExA, ToUnicodeEx, HC_ACTION,
     KBDLLHOOKSTRUCT, MSLLHOOKSTRUCT, WHEEL_DELTA, WH_KEYBOARD_LL, WH_MOUSE_LL, WM_KEYDOWN,
     WM_KEYUP, WM_LBUTTONDOWN, WM_LBUTTONUP, WM_MBUTTONDOWN, WM_MBUTTONUP, WM_MOUSEHWHEEL,
     WM_MOUSEMOVE, WM_MOUSEWHEEL, WM_RBUTTONDOWN, WM_RBUTTONUP, WM_XBUTTONDOWN, WM_XBUTTONUP,
+    VK_SHIFT, GetForegroundWindow, GetWindowThreadProcessId
 };
 
 fn default_callback(event: Event) {
     println!("Default : Event {:?}", event);
 }
 
+
+const TRUE: i32 = 1;
+const FALSE: i32 = 0;
+
 static mut GLOBAL_CALLBACK: Callback = default_callback;
 static mut HOOK: HHOOK = null_mut();
-static mut STATE: [u8; 256] = [0; 256];
 
 unsafe fn get_code(lpdata: isize) -> u32 {
     let kb = *(lpdata as *const KBDLLHOOKSTRUCT);
@@ -41,20 +48,99 @@ unsafe fn get_button_code(lpdata: isize) -> i32 {
     (mouse.mouseData as i32 >> 16) as i32
 }
 
+static mut LAST_CODE: u32 = 0;
+static mut LAST_SCAN_CODE: u32 = 0;
+static mut LAST_STATE: [u8; 256] = [0;256];
+static mut LAST_IS_DEAD: bool = false;
+
+unsafe fn get_name(lpdata: isize) -> Option<String>{
+    // https://gist.github.com/akimsko/2011327
+    // https://www.experts-exchange.com/questions/23453780/LowLevel-Keystroke-Hook-removes-Accents-on-French-Keyboard.html
+    let code = get_code(lpdata);
+    let scan_code = get_scan_code(lpdata);
+    let buff: [u16; 32] = [0; 32];
+    let buff_ptr = &buff as *const _ as *mut u16;
+    let mut state: [u8; 256] = [0; 256];
+    let state_ptr: *mut u8 = &mut state as *const _ as *mut u8;
+
+
+    let _shift = GetKeyState(VK_SHIFT);
+    let current_window_thread_id = GetWindowThreadProcessId(GetForegroundWindow(), null_mut());
+    let thread_id = GetCurrentThreadId();
+    // Attach to active thread so we can get that keyboard state
+    let status = if winuser::AttachThreadInput(thread_id, current_window_thread_id , TRUE) == 1{
+        // Current state of the modifiers in keyboard
+        let status = GetKeyboardState(state_ptr);
+
+        // Detach
+        winuser::AttachThreadInput(thread_id, current_window_thread_id, FALSE);
+        status
+    }else{
+        // Could not attach, perhaps it is this process?
+        GetKeyboardState(state_ptr)
+    };
+
+    if status != 1{
+        return None;
+    }
+    println!("Keyboard state status {:?}", status);
+    let layout = GetKeyboardLayout(current_window_thread_id);
+    let len = ToUnicodeEx(code, scan_code, state_ptr, buff_ptr, 8 - 1, 0, layout);
+    
+    let mut is_dead = false;
+    let result = if len > 0{
+        match String::from_utf16(&buff[0..len as usize]){
+            Ok(string) => {
+                Some(string)
+            },
+            Err(_) => None
+        }
+    }else if len == 0{
+        None
+    }else if len == -1{
+        is_dead = true;
+        clear_keyboard_buffer(code, scan_code, layout);
+        None
+    }else{
+        None
+    };
+
+
+    if LAST_CODE != 0 && LAST_IS_DEAD{
+        let mut buff: [u16; 32] = [0; 32];
+        let buff_ptr = &buff as *const _ as *mut u16;
+        let last_state_ptr = &LAST_STATE as *const _ as *mut u8;
+        ToUnicodeEx(LAST_CODE, LAST_SCAN_CODE, last_state_ptr, buff_ptr, 8-1, 0, layout);
+        LAST_CODE = 0;
+        return result
+    }
+
+    LAST_CODE = code;
+    LAST_SCAN_CODE = scan_code;
+    LAST_IS_DEAD = is_dead;
+    LAST_STATE.clone_from_slice(&state);
+
+    result
+}
+
+unsafe fn clear_keyboard_buffer(code: u32, scan_code: u32, layout: HKL){
+    let mut len = -1;
+    let mut buff: [u16; 32] = [0; 32];
+    let buff_ptr = &buff as *const _ as *mut u16;
+    let mut state: [u8; 256] = [0; 256];
+    let state_ptr: *mut u8 = &mut state as *const _ as *mut u8;
+
+    while len < 0{
+        len = ToUnicodeEx(code, scan_code, state_ptr, buff_ptr, 8 -1 , 0, layout);
+    }
+}
+
 unsafe extern "system" fn raw_callback(code: i32, param: usize, lpdata: isize) -> isize {
     if code == HC_ACTION {
         let opt = match param {
             x if x == WM_KEYDOWN as usize => {
                 let code = get_code(lpdata);
                 let key = key_from_code(code as u16);
-                let scan_code = get_scan_code(lpdata);
-                let buff: [u16; 32] = [0; 32];
-                let buff_ptr = &buff as *const _ as *mut u16;
-                let state_ptr = &mut STATE as *const _ as *mut u8;
-                GetKeyboardState(state_ptr);
-                // println!("Keyboard state {:?}", STATE);
-                ToUnicode(code, scan_code, state_ptr, buff_ptr, 8 - 1, 0);
-                println!("Buff {:?}", buff);
                 Some(EventType::KeyPress(key))
             }
             x if x == WM_KEYUP as usize => {
@@ -104,10 +190,16 @@ unsafe extern "system" fn raw_callback(code: i32, param: usize, lpdata: isize) -
         };
 
         if let Some(event_type) = opt {
+            let name = match &event_type{
+                EventType::KeyPress(_key) => {
+                    get_name(lpdata)
+                },
+                _ => None
+            };
             let event = Event {
                 event_type,
                 time: SystemTime::now(),
-                name: None,
+                name,
             };
             GLOBAL_CALLBACK(event);
         }
