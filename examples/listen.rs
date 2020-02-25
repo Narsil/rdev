59ef6997e4653fd3746ba3d71be97343376fd5ab
Nicolas Patry <patry.nicolas@protonmail.com>
2020-02-25 23:35:33 +0100

    Bumping version + last changes to doc.

--- examples/listen.rs
+++ examples/listen.rs
@@ -6,4 +6,8 @@ fn main() {
 
 fn callback(event: Event) {
     println!("My callback {:?}", event);
+    match event.name {
+        Some(string) => println!("User wrote {:?}", string),
+        None => (),
+    }
 }
