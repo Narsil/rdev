5185d72cced3c7bcd4c87590ccaaee1868bc59a3
Nicolas Patry <patry.nicolas@protonmail.com>
2020-02-26 09:24:32 +0100

    Adding global rust doc (copied from readme). + minor additions
    - Added crates and docs badges in README.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+//! Simple library to listen and send events to keyboard and mouse on MacOS, Windows and Linux
+//! (x11).
+//!
+//! You can also check out [Enigo](https://github.com/Enigo-rs/Enigo) which is another
+//! crate which helped me write this one.
+//!
+//! This crate is so far a pet project for me to understand the rust ecosystem.
+//!
+//! ## Simple Usage
+//!
+//! Listening to global events
+//!
+//! ```rust
+//! use rdev::{listen, Event};
+//!
+//! fn main() {
+//!     listen(callback);
+//! }
+//!
+//! fn callback(event: Event) {
+//!     println!("My callback {:?}", event);
+//!     match event.name {
+//!         Some(string) => println!("User wrote {:?}", string),
+//!         None => (),
+//!     }
+//! }
+//! ```
+//!
+//! Sending some events
+//!
+//! ```rust
+//! use rdev::{simulate, Button, EventType, Key, SimulateError};
+//! use std::{thread, time};
+//!
+//! fn send(event_type: &EventType) {
+//!     let delay = time::Duration::from_millis(20);
+//!     match simulate(event_type) {
+//!         Ok(()) => (),
+//!         Err(SimulateError) => {
+//!             println!("We could not send {:?}", event_type);
+//!         }
+//!     }
+//!     // Let ths OS catchup (at least MacOS)
+//!     thread::sleep(delay);
+//! }
+//!
+//! fn main() {
+//!     send(&EventType::KeyPress(Key::KeyS));
+//!     send(&EventType::KeyRelease(Key::KeyS));
+//!
+//!     send(&EventType::MouseMove { x: 0.0, y: 0.0 });
+//!     send(&EventType::MouseMove { x: 400.0, y: 400.0 });
+//!     send(&EventType::ButtonPress(Button::Left));
+//!     send(&EventType::ButtonRelease(Button::Right));
+//!     send(&EventType::Wheel {
+//!         delta_x: 0,
+//!         delta_y: 1,
+//!     });
+//! }
+//! ```
 mod rdev;
 pub use crate::rdev::{Button, Callback, Event, EventType, Key, SimulateError};
 
