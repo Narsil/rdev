a69317e63f3f64fd1cf15ddaec92a33edf17276a
Max Blachman <blachmanmax@gmail.com>
2020-03-28 06:36:10 -0700

    change rust types to core_graphics types (macOS)
    change keycodes type to CGKeyCode in an attempt to improve portability

--- src/macos/keycodes.rs
+++ src/macos/keycodes.rs
@@ -1,87 +1,89 @@
 use crate::rdev::Key;
+use core_graphics::event::CGKeyCode;
+use std::convert::TryInto;
 
 /// Option
-const ALT: u32 = 58;
+const ALT: CGKeyCode = 58;
 /// Option_Right
-const ALT_GR: u32 = 61;
-const BACKSPACE: u32 = 51;
-const CAPS_LOCK: u32 = 57;
+const ALT_GR: CGKeyCode = 61;
+const BACKSPACE: CGKeyCode = 51;
+const CAPS_LOCK: CGKeyCode = 57;
 /// Control Right does not exist on Mac
-const CONTROL_LEFT: u32 = 59;
-const DOWN_ARROW: u32 = 125;
-const ESCAPE: u32 = 53;
-const F1: u32 = 122;
-const F10: u32 = 109;
-const F11: u32 = 103;
-const F12: u32 = 111;
-const F2: u32 = 120;
-const F3: u32 = 99;
-const F4: u32 = 118;
-const F5: u32 = 96;
-const F6: u32 = 97;
-const F7: u32 = 98;
-const F8: u32 = 100;
-const F9: u32 = 101;
-const FUNCTION: u32 = 63;
-const LEFT_ARROW: u32 = 123;
-const META_LEFT: u32 = 55;
-const META_RIGHT: u32 = 54;
-const RETURN: u32 = 36;
-const RIGHT_ARROW: u32 = 124;
-const SHIFT_LEFT: u32 = 56;
-const SHIFT_RIGHT: u32 = 60;
-const SPACE: u32 = 49;
-const TAB: u32 = 48;
-const UP_ARROW: u32 = 126;
-const BACK_QUOTE: u32 = 50;
-const NUM1: u32 = 18;
-const NUM2: u32 = 19;
-const NUM3: u32 = 20;
-const NUM4: u32 = 21;
-const NUM5: u32 = 23;
-const NUM6: u32 = 22;
-const NUM7: u32 = 26;
-const NUM8: u32 = 28;
-const NUM9: u32 = 25;
-const NUM0: u32 = 29;
-const MINUS: u32 = 27;
-const EQUAL: u32 = 24;
-const KEY_Q: u32 = 12;
-const KEY_W: u32 = 13;
-const KEY_E: u32 = 14;
-const KEY_R: u32 = 15;
-const KEY_T: u32 = 17;
-const KEY_Y: u32 = 16;
-const KEY_U: u32 = 32;
-const KEY_I: u32 = 34;
-const KEY_O: u32 = 31;
-const KEY_P: u32 = 35;
-const LEFT_BRACKET: u32 = 33;
-const RIGHT_BRACKET: u32 = 30;
-const KEY_A: u32 = 0;
-const KEY_S: u32 = 1;
-const KEY_D: u32 = 2;
-const KEY_F: u32 = 3;
-const KEY_G: u32 = 5;
-const KEY_H: u32 = 4;
-const KEY_J: u32 = 38;
-const KEY_K: u32 = 40;
-const KEY_L: u32 = 37;
-const SEMI_COLON: u32 = 41;
-const QUOTE: u32 = 39;
-const BACK_SLASH: u32 = 42;
-const KEY_Z: u32 = 6;
-const KEY_X: u32 = 7;
-const KEY_C: u32 = 8;
-const KEY_V: u32 = 9;
-const KEY_B: u32 = 11;
-const KEY_N: u32 = 45;
-const KEY_M: u32 = 46;
-const COMMA: u32 = 43;
-const DOT: u32 = 47;
-const SLASH: u32 = 44;
+const CONTROL_LEFT: CGKeyCode = 59;
+const DOWN_ARROW: CGKeyCode = 125;
+const ESCAPE: CGKeyCode = 53;
+const F1: CGKeyCode = 122;
+const F10: CGKeyCode = 109;
+const F11: CGKeyCode = 103;
+const F12: CGKeyCode = 111;
+const F2: CGKeyCode = 120;
+const F3: CGKeyCode = 99;
+const F4: CGKeyCode = 118;
+const F5: CGKeyCode = 96;
+const F6: CGKeyCode = 97;
+const F7: CGKeyCode = 98;
+const F8: CGKeyCode = 100;
+const F9: CGKeyCode = 101;
+const FUNCTION: CGKeyCode = 63;
+const LEFT_ARROW: CGKeyCode = 123;
+const META_LEFT: CGKeyCode = 55;
+const META_RIGHT: CGKeyCode = 54;
+const RETURN: CGKeyCode = 36;
+const RIGHT_ARROW: CGKeyCode = 124;
+const SHIFT_LEFT: CGKeyCode = 56;
+const SHIFT_RIGHT: CGKeyCode = 60;
+const SPACE: CGKeyCode = 49;
+const TAB: CGKeyCode = 48;
+const UP_ARROW: CGKeyCode = 126;
+const BACK_QUOTE: CGKeyCode = 50;
+const NUM1: CGKeyCode = 18;
+const NUM2: CGKeyCode = 19;
+const NUM3: CGKeyCode = 20;
+const NUM4: CGKeyCode = 21;
+const NUM5: CGKeyCode = 23;
+const NUM6: CGKeyCode = 22;
+const NUM7: CGKeyCode = 26;
+const NUM8: CGKeyCode = 28;
+const NUM9: CGKeyCode = 25;
+const NUM0: CGKeyCode = 29;
+const MINUS: CGKeyCode = 27;
+const EQUAL: CGKeyCode = 24;
+const KEY_Q: CGKeyCode = 12;
+const KEY_W: CGKeyCode = 13;
+const KEY_E: CGKeyCode = 14;
+const KEY_R: CGKeyCode = 15;
+const KEY_T: CGKeyCode = 17;
+const KEY_Y: CGKeyCode = 16;
+const KEY_U: CGKeyCode = 32;
+const KEY_I: CGKeyCode = 34;
+const KEY_O: CGKeyCode = 31;
+const KEY_P: CGKeyCode = 35;
+const LEFT_BRACKET: CGKeyCode = 33;
+const RIGHT_BRACKET: CGKeyCode = 30;
+const KEY_A: CGKeyCode = 0;
+const KEY_S: CGKeyCode = 1;
+const KEY_D: CGKeyCode = 2;
+const KEY_F: CGKeyCode = 3;
+const KEY_G: CGKeyCode = 5;
+const KEY_H: CGKeyCode = 4;
+const KEY_J: CGKeyCode = 38;
+const KEY_K: CGKeyCode = 40;
+const KEY_L: CGKeyCode = 37;
+const SEMI_COLON: CGKeyCode = 41;
+const QUOTE: CGKeyCode = 39;
+const BACK_SLASH: CGKeyCode = 42;
+const KEY_Z: CGKeyCode = 6;
+const KEY_X: CGKeyCode = 7;
+const KEY_C: CGKeyCode = 8;
+const KEY_V: CGKeyCode = 9;
+const KEY_B: CGKeyCode = 11;
+const KEY_N: CGKeyCode = 45;
+const KEY_M: CGKeyCode = 46;
+const COMMA: CGKeyCode = 43;
+const DOT: CGKeyCode = 47;
+const SLASH: CGKeyCode = 44;
 
-pub fn code_from_key(key: Key) -> Option<u32> {
+pub fn code_from_key(key: Key) -> Option<CGKeyCode> {
     match key {
         Key::Alt => Some(ALT),
         Key::AltGr => Some(ALT_GR),
@@ -160,12 +162,12 @@ pub fn code_from_key(key: Key) -> Option<u32> {
         Key::Dot => Some(DOT),
         Key::Slash => Some(SLASH),
         Key::Function => Some(FUNCTION),
-        Key::Unknown(code) => Some(code),
+        Key::Unknown(code) => code.try_into().ok(),
         _ => None,
     }
 }
 
-pub fn key_from_code(code: u32) -> Key {
+pub fn key_from_code(code: CGKeyCode) -> Key {
     match code {
         ALT => Key::Alt,
         ALT_GR => Key::AltGr,
@@ -244,7 +246,7 @@ pub fn key_from_code(code: u32) -> Key {
         DOT => Key::Dot,
         SLASH => Key::Slash,
         FUNCTION => Key::Function,
-        code => Key::Unknown(code),
+        code => Key::Unknown(code.into()),
     }
 }
 
@@ -253,12 +255,11 @@ mod test {
     use super::{code_from_key, key_from_code};
     #[test]
     fn test_reversible() {
-        for code in 0..65636 {
+        for code in 0..=65535 {
             let key = key_from_code(code);
-            if let Some(code2) = code_from_key(key) {
-                assert_eq!(code, code2)
-            } else {
-                assert!(false, "We could not convert back code: {:?}", code);
+            match code_from_key(key) {
+                Some(code2) => assert_eq!(code, code2),
+                None => panic!("Could not convert back code: {:?}", code),
             }
         }
     }
--- src/macos/simulate.rs
+++ src/macos/simulate.rs
@@ -1,76 +1,58 @@
 use crate::rdev::{Button, EventType, SimulateError};
 use core_graphics::event::{
-    CGEvent, CGEventTapLocation, CGEventType, CGKeyCode, CGMouseButton, ScrollEventUnit,
+    CGEvent, CGEventTapLocation, CGEventType, CGMouseButton, ScrollEventUnit,
 };
 use core_graphics::event_source::{CGEventSource, CGEventSourceStateID};
 use core_graphics::geometry::CGPoint;
+use std::convert::TryInto;
 
 use crate::macos::keycodes::code_from_key;
 
 unsafe fn convert_native_with_source(
     event_type: &EventType,
     source: CGEventSource,
-) -> Result<CGEvent, ()> {
+) -> Option<CGEvent> {
     match event_type {
         EventType::KeyPress(key) => {
-            if let Some(code) = code_from_key(*key) {
-                return CGEvent::new_keyboard_event(source, code as CGKeyCode, true);
-            }
-            Err(())
+            CGEvent::new_keyboard_event(source, code_from_key(*key)?, true).ok()
         }
         EventType::KeyRelease(key) => {
-            if let Some(code) = code_from_key(*key) {
-                return CGEvent::new_keyboard_event(source, code as CGKeyCode, false);
-            }
-            Err(())
+            CGEvent::new_keyboard_event(source, code_from_key(*key)?, false).ok()
         }
         EventType::ButtonPress(button) => {
-            let point = get_current_mouse_location();
-            match button {
-                Button::Left => {
-                    CGEvent::new_mouse_event(
-                        source,
-                        CGEventType::LeftMouseDown,
-                        point,
-                        CGMouseButton::Left, // This is actually ignored because we don't use OtherMouse EventType
-                    )
-                }
-                Button::Right => {
-                    CGEvent::new_mouse_event(
-                        source,
-                        CGEventType::RightMouseDown,
-                        point,
-                        CGMouseButton::Left, // This is actually ignored because we don't use OtherMouse EventType
-                    )
-                }
-                _ => Err(()),
-            }
+            let point = get_current_mouse_location()?;
+            let event = match button {
+                Button::Left => CGEventType::LeftMouseDown,
+                Button::Right => CGEventType::RightMouseDown,
+                _ => return None,
+            };
+            CGEvent::new_mouse_event(
+                source,
+                event,
+                point,
+                CGMouseButton::Left, // ignored because we don't use OtherMouse EventType
+            ).ok()
         }
         EventType::ButtonRelease(button) => {
-            let point = get_current_mouse_location();
-            match button {
-                Button::Left => {
-                    CGEvent::new_mouse_event(
-                        source,
-                        CGEventType::LeftMouseUp,
-                        point,
-                        CGMouseButton::Left, // This is actually ignored because we don't use OtherMouse EventType
-                    )
-                }
-                Button::Right => {
-                    CGEvent::new_mouse_event(
-                        source,
-                        CGEventType::RightMouseUp,
-                        point,
-                        CGMouseButton::Left, // This is actually ignored because we don't use OtherMouse EventType
-                    )
-                }
-                _ => Err(()),
-            }
+            let point = get_current_mouse_location()?;
+            let event = match button {
+                Button::Left => CGEventType::LeftMouseUp,
+                Button::Right => CGEventType::RightMouseUp,
+                _ => return None,
+            };
+            CGEvent::new_mouse_event(
+                source,
+                event,
+                point,
+                CGMouseButton::Left, // ignored because we don't use OtherMouse EventType
+            ).ok()
         }
         EventType::MouseMove { x, y } => {
-            let point = CGPoint { x: *x, y: *y };
-            CGEvent::new_mouse_event(source, CGEventType::MouseMoved, point, CGMouseButton::Left)
+            let point = CGPoint {
+                x: (*x).try_into().ok()?,
+                y: (*y).try_into().ok()?,
+            };
+            CGEvent::new_mouse_event(source, CGEventType::MouseMoved, point, CGMouseButton::Left).ok()
         }
         EventType::Wheel { delta_x, delta_y } => {
             let wheel_count = 2;
@@ -78,35 +60,29 @@ unsafe fn convert_native_with_source(
                 source,
                 ScrollEventUnit::PIXEL,
                 wheel_count,
-                *delta_y as i32,
-                *delta_x as i32,
+                (*delta_y).try_into().ok()?,
+                (*delta_x).try_into().ok()?,
                 0,
-            )
+            ).ok()
         }
     }
 }
 
-unsafe fn convert_native(event_type: &EventType) -> Result<CGEvent, ()> {
-    match CGEventSource::new(CGEventSourceStateID::HIDSystemState) {
-        Ok(source) => convert_native_with_source(event_type, source),
-        Err(_) => Err(()),
-    }
+unsafe fn convert_native(event_type: &EventType) -> Option<CGEvent> {
+    let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).ok()?;
+    convert_native_with_source(event_type, source)
 }
 
-unsafe fn get_current_mouse_location() -> CGPoint {
-    match CGEventSource::new(CGEventSourceStateID::HIDSystemState) {
-        Ok(source) => match CGEvent::new(source) {
-            Ok(event) => event.location(),
-            Err(_) => CGPoint { x: -1.0, y: -1.0 },
-        },
-        Err(_) => CGPoint { x: -1.0, y: -1.0 },
-    }
+unsafe fn get_current_mouse_location() -> Option<CGPoint> {
+    let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).ok()?;
+    let event = CGEvent::new(source).ok()?;
+    Some(event.location())
 }
 
 #[link(name = "Cocoa", kind = "framework")]
 pub fn simulate(event_type: &EventType) -> Result<(), SimulateError> {
     unsafe {
-        if let Ok(cg_event) = convert_native(event_type) {
+        if let Some(cg_event) = convert_native(event_type) {
             cg_event.post(CGEventTapLocation::HID);
             Ok(())
         } else {
