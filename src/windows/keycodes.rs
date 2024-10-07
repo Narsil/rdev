b3d58a814174e9faede2895a9d35afa9a0e17889
Jonathan <1808569+jonatino@users.noreply.github.com>
2024-10-07 16:03:44 -0400

    add volume keycodes for windows

--- src/windows/keycodes.rs
+++ src/windows/keycodes.rs
@@ -132,7 +132,13 @@ decl_keycodes! {
     Kp7, 103,
     Kp8, 104,
     Kp9, 105,
-    KpDelete, 110
+    KpDelete, 110,
+    VolumeMute: 173,
+    VolumeDown: 174,
+    VolumeUp: 175,
+    NextTrack: 176,
+    PreviousTrack: 177,
+    PlayPause: 179,
 }
 
 #[cfg(test)]
