fed5ec89fa8cba86c1eb6718abb01be239ab03ae
Nicolas Patry <patry.nicolas@protonmail.com>
2025-05-05 13:22:25 +0200

    Semi working state.

--- src/linux/wayland/keyboard.rs
+++ src/linux/wayland/keyboard.rs
@@ -1,7 +1,16 @@
-use crate::rdev::{EventType, KeyboardState};
+use crate::{
+    rdev::{EventType, KeyboardState},
+    SimulateError,
+};
 
 #[derive(Debug)]
 pub struct Keyboard {}
+
+impl Keyboard {
+    pub fn new() -> Result<Self, SimulateError> {
+        Ok(Self {})
+    }
+}
 
 impl KeyboardState for Keyboard {
     fn add(&mut self, _event_type: &EventType) -> Option<String> {
